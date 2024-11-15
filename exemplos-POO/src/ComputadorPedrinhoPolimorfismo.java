db92b1cdd40bf8495cbb3ed4e094d67c71ebad22
Daniel01ricardo <daniel01ricardo@hotmail.com>
2024-11-14 23:48:08 -0300

    Segundo commit, adicionar metodo com polimorfismo

---
+++ exemplos-POO/src/ComputadorPedrinhoPolimorfismo.java
@@ -0,0 +1,22 @@
+import aplicativos.FacebookMessenger;
+import aplicativos.MSNMessenger;
+import aplicativos.Telegram;
+import services.ServicoMensagemInstantanea;
+
+public class ComputadorPedrinhoPolimorfismo {
+	public static void Main(String[] args) {
+		ServicoMensagemInstantanea smi = null;
+		
+		String appEscolhido="tlg";
+		
+		if(appEscolhido.equals("msn"))
+			smi = new MSNMessenger();
+		else if(appEscolhido.equals("fbm"))
+			smi = new FacebookMessenger();
+		else if(appEscolhido.equals("tlg"))
+			smi = new Telegram();
+		
+		smi.enviarMensagemABS();
+		smi.receberMensagemABS();
+	}
+}
